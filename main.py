1a1b85757113a3e864b9a0eed537ab63b3d46326
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-03-09 14:32:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ class ChemicalWaste:
                       'lc50 fish',
                       'ld50 dermal',
                       'ld50 oral']
+    
+    valid_concentration = list(range(101))
 
     def __init__(self, name, type_of_tox_test, tox_test_result):
         self.name = name
         self.type_of_tox_test = type_of_tox_test
         self.tox_test_result = tox_test_result
-
 
