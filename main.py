2e5619ac469fad65f8f8448609d8e47418e4d7ee
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-03-09 14:22:52 -0800

    Update main.py
    create class for a chemical waste that is being evaluated

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 # create eq conc calculator
 
+
+class ChemicalWaste:
+
+    valid_tox_test = ['lc50 inh ',
+                      'lc50 fish',
+                      'ld50 dermal',
+                      'ld50 oral']
+
+    def __init__(self, name, type_of_tox_test, tox_test_result):
+        self.name = name
+        self.type_of_tox_test = type_of_tox_test
+        self.tox_test_result = tox_test_result
+
+        
