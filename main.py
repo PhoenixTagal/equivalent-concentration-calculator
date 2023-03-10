bf924c3ce3cd4940afac50c9051043fb3a58522b
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-03-10 10:24:30 -0800

    add tox magnitude dict
    created dict to reference tox category magnitude to calculate equivalent concentration

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ class ChemicalWaste:
                       'ld50 dermal': [(2, 20), (20, 200), (200, 2000), (2000, 20000)],
                       'ld50 oral': [(0.5, 5), (5, 50), (50, 500), (500, 5000)]}
 
+    tox_class_magnitude = {'x': 1,
+                           'a': 10,
+                           'b': 100,
+                           'c': 1000,
+                           'd': 10000}
+
     # percent composition must be an int between 0-100
     valid_percent_composition = list(range(101))
 
+    # name arg must equal a key from valid_tox_test dict
     def __init__(self, name, type_of_tox_test, tox_test_result, percent_composition):
         self.name = name
         self.type_of_tox_test = type_of_tox_test
