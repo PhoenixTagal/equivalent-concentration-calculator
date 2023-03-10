bcc722fe94c978395c9d5c1bf64772431e68dbae
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-03-10 10:45:46 -0800

    created seperate .py file
    created new file to hold criteria data to clean up main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 # create eq conc calculator
 
-valid_tox_test = {'lc50 inh': [(0.02, 0.2), (0.2, 2), (2, 20), (20, 200)],
-                  'lc50 fish': [(0.01, 0.1), (0.1, 1), (1, 10), (10, 100)],
-                  'ld50 dermal': [(2, 20), (20, 200), (200, 2000), (2000, 20000)],
-                  'ld50 oral': [(0.5, 5), (5, 50), (50, 500), (500, 5000)]}
-
-tox_class_magnitude = {'x': 1,
-                       'a': 10,
-                       'b': 100,
-                       'c': 1000,
-                       'd': 10000}
-
-valid_percent_composition = list(range(101))
+from tox_eval_criteria import valid_tox_test, valid_percent_composition, tox_class_magnitude
 
 class ChemicalWaste:
     # name arg must be equal to a key from valid_tox_test dict
