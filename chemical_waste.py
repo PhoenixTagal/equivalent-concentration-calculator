664f820be5b7df9745649092ec287578a7d6e232
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-03-10 10:50:51 -0800

    new files
    created test.py
    
    refactored main.py to chemical_waste.py
    allow for cleaner testing and dev

--- chemical_waste.py
+++ chemical_waste.py
@@ -1,7 +1,3 @@
-# create eq conc calculator
-
-from tox_eval_criteria import valid_tox_test, valid_percent_composition, tox_class_magnitude
-
 class ChemicalWaste:
     # name arg must be equal to a key from valid_tox_test dict
     def __init__(self, name, type_of_tox_test, tox_test_result, percent_composition):
@@ -14,9 +10,6 @@ class ChemicalWaste:
         pass
 
 
-hcl = ChemicalWaste(name='hcl', type_of_tox_test='ld50 oral', tox_test_result= 4000,
-                    percent_composition=50)
 
-print(valid_tox_test[hcl.type_of_tox_test])
 
 
---
+++ test.py
@@ -0,0 +1,7 @@
+from chemical_waste import ChemicalWaste
+from tox_eval_criteria import valid_tox_test, valid_percent_composition, tox_class_magnitude
+
+# test obj
+hcl = ChemicalWaste(name='hcl', type_of_tox_test='ld50 oral', tox_test_result=4000,
+                    percent_composition=50)
+
