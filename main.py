e52fcf1e3d4046ea97b6ed0cbcbad0ca65797178
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-03-10 10:15:01 -0800

    input tox eval criteria
    created dict to store all eval data.
    
    methods will reference valid_tox_test dict for tox evaluations

--- main.py
+++ main.py
@@ -2,16 +2,17 @@
 
 
 class ChemicalWaste:
+    # str of one of the four tox tests
+    valid_tox_test = {'lc50 inh': [(0.02, 0.2), (0.2, 2), (2, 20), (20, 200)],
+                      'lc50 fish': [(0.01, 0.1), (0.1, 1), (1, 10), (10, 100)],
+                      'ld50 dermal': [(2, 20), (20, 200), (200, 2000), (2000, 20000)],
+                      'ld50 oral': [(0.5, 5), (5, 50), (50, 500), (500, 5000)]}
 
-    valid_tox_test = ['lc50 inh',
-                      'lc50 fish',
-                      'ld50 dermal',
-                      'ld50 oral']
-    
-    valid_concentration = list(range(101))
+    # concentration % must be int between 0-100
+    valid_percent_composition = list(range(101))
 
-    def __init__(self, name, type_of_tox_test, tox_test_result):
+    def __init__(self, name, type_of_tox_test, tox_test_result, percent_composition):
         self.name = name
         self.type_of_tox_test = type_of_tox_test
         self.tox_test_result = tox_test_result
-
+        self.percent_composition = percent_composition
