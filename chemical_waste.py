6b3d75333528861b1d8be05dffc6e483a68e1da9
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-03-10 12:45:29 -0800

    testing
    working through method development for ChemicalWaste in test.py with a test ChemicalWaste obj

--- chemical_waste.py
+++ chemical_waste.py
@@ -1,5 +1,8 @@
+from tox_eval_criteria import valid_tox_test, valid_percent_composition, tox_class_magnitude
+
+
 class ChemicalWaste:
-    # name arg must be equal to a key from valid_tox_test dict
+    # type_of_tox_test arg must be equal to a key from valid_tox_test dict
     def __init__(self, name, type_of_tox_test, tox_test_result, percent_composition):
         self.name = name
         self.type_of_tox_test = type_of_tox_test
@@ -7,9 +10,21 @@ class ChemicalWaste:
         self.percent_composition = percent_composition
 
     def equivalent_concentration(self):
-        pass
+        evaluation_list = valid_tox_test[self.type_of_tox_test]
+        return evaluation_list
 
-
-
-
-
+    # def equivalent_concentration(self):
+    #     evaluation_list = valid_tox_test[self.type_of_tox_test]
+    #     criteria = []
+    #     for i in evaluation_list:
+    #         criteria.append(i)
+    #
+    #     tox_cat = {'x': criteria[0][0],
+    #                'a': criteria[0],
+    #                'b': criteria[1],
+    #                'c': criteria[2],
+    #                'd': criteria[3],
+    #                'non-tox': criteria[3][1]}
+    #
+    #     if self.tox_test_result > tox_cat['non-tox']:
+    #         tox_magnitude = tox_class_magnitude['d']
--- test.py
+++ test.py
@@ -5,3 +5,19 @@ from tox_eval_criteria import valid_tox_test, valid_percent_composition, tox_cla
 hcl = ChemicalWaste(name='hcl', type_of_tox_test='ld50 oral', tox_test_result=4000,
                     percent_composition=50)
 
+data = hcl.equivalent_concentration()
+
+criteria = []
+for i in data:
+    criteria.append(i)
+
+tox_cat = {'x': criteria[0][0],
+           'a': criteria[0],
+           'b': criteria[1],
+           'c': criteria[2],
+           'd': criteria[3],
+           'non-tox': criteria[3][1]}
+
+if hcl.tox_test_result > tox_cat['non-tox']:
+    tox_magnitude = tox_class_magnitude['d']
+elif hcl.tox_test_result
