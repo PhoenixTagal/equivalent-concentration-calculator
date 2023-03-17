a3ba5a415bd941347f313887371444d6c8fcd3d5
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-03-17 10:29:27 -0700

    GUI
    created new .py to create a quick GUI

---
+++ gui_root.py
@@ -0,0 +1,10 @@
+"""GUI for quick use of tox calculator to. Designed to use while
+completing the hazardous waste designation form. The results from this form can be
+used to fill out the associated fields"""
+
+
+import customtkinter
+from chemical_waste import *
+
+
+
--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 from chemical_waste import ChemicalWaste
 
 # test obj
-buffer = ChemicalWaste(name='buffer', type_of_tox_test='lc50 inh', tox_test_result=0.16,
-                       percent_composition=3)
+guanidine = ChemicalWaste(name='guanine', type_of_tox_test='ld50 oral', tox_test_result=282,
+                       percent_composition=95)
 
-print(buffer.designate_waste())
+triton = ChemicalWaste(name='triton', type_of_tox_test='ld50 oral', tox_test_result=1900,
+                       percent_composition=24)
+
+etoh = ChemicalWaste('etoh', 'lc50 inh', 117, 100)
+
+print(guanidine.designate_waste())
+print(' ')
+print(triton.designate_waste())
+print(' ')
+print(etoh.designate_waste())
