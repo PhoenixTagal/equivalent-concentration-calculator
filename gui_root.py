80075612280bc5a0ee987029db9f03adf54bb350
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-03-17 10:31:44 -0700

    Update gui_root.py
    Fixed spelling

--- gui_root.py
+++ gui_root.py
@@ -1,6 +1,6 @@
-"""GUI for quick use of tox calculator to. Designed to use while
-completing the hazardous waste designation form. The results from this form can be
-used to fill out the associated fields"""
+"""GUI for quick use of tox calculator. Designed to be used while
+completing the hazardous waste designation form. The results from this calculator can be
+used to fill out the associated fields in the designation form."""
 
 
 import customtkinter
